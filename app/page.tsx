6a863c68f25dc4865386c35e00cf134ab0117155
noahwekesa <noahwekesa0@gmail.com>
2024-02-18 21:20:05 +0300

    hero done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
 import Navbar from "../components/navbar";
+import { Spotlight } from "../components/ui/Spotlight";
 
 export default function Home() {
   return (
-    <div className="w-full  md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+    <div className="w-full h-screen md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <Navbar />
+      <Spotlight className="hidden md:flex md:-top-80 left-80  " fill="white" />
+      <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-32 px-2">
+        <div
+          className="text-4xl pb-5 md:text-7xl px-6 text-center  bg-clip-text text-transparent 
+             bg-gradient-to-b from-neutral-50
+              to bg-neutral-400 bg-opacity-50"
+        >
+          WELCOME <br /> LAURA SALOON
+        </div>
+        <p className="mt-4 text-lg font-normal  text-neutral-300 max-w-lg text-center mx-auto px-4">
+          Your favourite hair stylist
+        </p>
+        <Link
+          href={"/"}
+          className="cursor-pointer flex items-center justify-center border rounded-full w-48 p-2 mx-auto my-6 text-white"
+        >
+          BOOK NOW
+        </Link>
+      </div>
     </div>
   );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,19 +27,19 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="cursor-pointer hidden md:flex space-x-10 items-center text-slate-300 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to bg-neutral-400 bg-opacity-50">
-          <Link href="/team" className="hover:text-gray-50">
+          <Link href="/" className="hover:text-gray-50">
             OUR TEAM
           </Link>
-          <Link href="/team" className="hover:text-gray-50">
+          <Link href="/" className="hover:text-gray-50">
             STYLES
           </Link>
-          <Link href="/team" className="hover:text-gray-50">
+          <Link href="/" className="hover:text-gray-50">
             SPA
           </Link>
-          <Link href="/team" className="hover:text-gray-50">
+          <Link href="/" className="hover:text-gray-50">
             JOIN OUR TEAM
           </Link>
-          <Link href="/pricing" className="hover:text-gray-50">
+          <Link href="/" className="hover:text-gray-50">
             PRICING
           </Link>
         </div>
@@ -56,6 +56,22 @@ const Navbar = () => {
               className="w-8 h-8 text-slate-300"
             />
           )}
+        </div>
+
+        <div className="hidden md:flex">
+          <Link
+            href="/contact"
+            className="
+            rounded inline-flex h-12 animate-shimmer items-center justify-center 
+            rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] 
+            bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors
+             focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2
+              focus:ring-offset-slate-50
+
+            "
+          >
+            Contact
+          </Link>
         </div>
       </div>
     </div>
