ef547afabd6053ec260bd88095ab4ba2d2034db7
noahwekesa <noahwekesa0@gmail.com>
2024-02-19 19:53:42 +0300

    working hours

--- app/WorkingHours.tsx
+++ app/WorkingHours.tsx
@@ -2,12 +2,12 @@
 
 const WorkingHours = () => {
   return (
-    <div className="text-white items-center m-20">
+    <div className="text-white items-center m-10">
       <h2 className="text-center text-2xl underline"> Working Hours</h2>
 
       <div className="relative overflow-x-auto">
-        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <table className="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-white uppercase">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Day
@@ -17,6 +17,20 @@ const WorkingHours = () => {
               </th>
             </tr>
           </thead>
+          <tbody>
+            <tr>
+              <th className="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
+                MONDAY
+              </th>
+              <td className="px-6 py-4 font-medium text-white">9am To 5pm</td>
+            </tr>
+            <tr>
+              <th className="px-6 py-4 font-medium whitespace-nowrap dark:text-white">
+                TUESDAY
+              </th>
+              <td className="px-6 py-4 font-medium text-white">9am To 5pm</td>
+            </tr>
+          </tbody>
         </table>
       </div>
     </div>
